31bb70b61cb56984937053dcd4b462ba6137985f
viator1 <vbc950109@gmail.com>
2023-01-04 11:13:21 +0900

    게시판

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "secondapp",
     "accounts",
     "file",
+    "board",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -80,8 +81,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mbti',
+        'USER': 'mbti',
+        'PASSWORD': '1234',
+        'HOST': '15.164.153.191',
+        'PORT': 3306
     }
 }
 
